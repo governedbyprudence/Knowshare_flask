344fba110be3e4369f27849a0d2dcc67b9d964cb
ThUnDeR-CoDeR123 <106828462+ThUnDeR-CoDeR123@users.noreply.github.com>
2023-07-30 23:55:12 +0530

    jwt created

--- app/admin/setup.py
+++ app/admin/setup.py
@@ -1,19 +1,19 @@
-import environ
-import os
+# # import environ
+# import os
 
 
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
-)
+# env = environ.Env(
+#     # set casting, default value
+#     DEBUG=(bool, False)
+# )
 
 
-# Set the project base directory
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# # Set the project base directory
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
-DEBUG = env('DEBUG')
-SECRET_KEY = env('SECRET_KEY')
-SQLALCHEMY_DATABASE_URI=env('SQLALCHEMY_DATABASE_URI')
+# # Take environment variables from .env file
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+# DEBUG = env('DEBUG')
+# SECRET_KEY = env('SECRET_KEY')
+# SQLALCHEMY_DATABASE_URI=env('SQLALCHEMY_DATABASE_URI')
--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 from blueprints.user import blueprint_signup,blueprint_login,blueprint_getuserlst
 from db.database import db
-from admin.setup import SECRET_KEY, SQLALCHEMY_DATABASE_URI
+# from admin.setup import SECRET_KEY, SQLALCHEMY_DATABASE_URI
 
 
 app=Flask(__name__)
 UPLOAD_FOLDER = './db'
 ALLOWED_EXTENSIONS = {'pdf'}
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config['SQLALCHEMY_DATABASE_URI']=SQLALCHEMY_DATABASE_URI
+app.config['SECRET_KEY'] = "d55cc5e7c5c840b29eac8831d43203da"
+app.config['SQLALCHEMY_DATABASE_URI']="postgresql://ecelilmw:Eb9eNRBLa1rQeA1O6eSAZjRC9BUJwDBm@tiny.db.elephantsql.com/ecelilmw"
 app.config['SQLALCHEMY_TRACK_MODIFICATINOS'] = True
 db.init_app(app)
 
